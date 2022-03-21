dd7085220b6d22a30236ae5ca96548f07275d34b
aafusam <62737721+aafusam@users.noreply.github.com>
2022-03-21 12:16:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
 
 TOKEN = os.environ.get("TOKEN", "")
-OWNER = os.environ.get("OWNER", "AafuSam")
+OWNER = os.environ.get("OWNER", "AafuSam013")
 GROUP = os.environ.get("GROUP", "StarterMovies")
 CHANNEL = os.environ.get("CHANNEL", "StarterNetworkz")
 BOT_NAME = os.environ.get("BOT_NAME", "AnonySam-ChatBot")
